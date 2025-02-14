ac414cf70446a411c3514b1c9ae4e948aa39f51b
Jonah Wack <2330034@collegebart.ca>
2025-02-14 11:10:39 -0500

    labo 1 et 2

--- Labo1/Labo1/main.cpp
+++ Labo1/Labo1/main.cpp
@@ -116,10 +116,10 @@ void exercice6() {
 	}
 }
 void exercice7() {
-	int Ee = 6;
-	int Ei = 2;
+	int Ee = 5;
+	int Ei = 1;
 	int a = 0;
-	for (int i = 1; i <= Ee; i++) {
+	for (int i = 1; i < Ee; i++) {
 		std::cout << " ";
 	}
 	std::cout << "*" << std::endl;
@@ -133,24 +133,86 @@ void exercice7() {
 			std::cout << " ";
 		}
 		std::cout << "*" << std::endl;
-		Ee = Ee - 2;
-		Ei = Ei + 4;
+		Ee = Ee - 1;
+		Ei = Ei + 2;
+	}
+	while (Ei > 0) {
+		for (int i = 0; i < Ee; i++) {
+			std::cout << " ";
+		}
+		std::cout << "*";
+		for (int i = 0; i < Ei; i++) {
+			std::cout << " ";
+		}
+		std::cout << "*" << std::endl;
+		Ee = Ee + 1;
+		Ei = Ei - 2;
+	}
+	for (int i = 1; i < Ee+1; i++) {
+		std::cout << " ";
+	}
+	std::cout << "*" << std::endl;
+}
+
+void exercice8() {
+	int space = 2;
+	int star = 1;
+	while (space >= 0)
+	{
+		for (int i = 0; i < space; i++) {
+			std::cout << " ";
+		}
+		for (int i = 0; i < star; i++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+		space -= 1;
+		star += 2;
+	}
+	space = 2;
+	for (int i = 0; i < 6; i++) {
+		for (int y = 0; y < space; y++) {
+			std::cout << " ";
+		}
+		std::cout << "*" << std::endl;
 	}
 }
 
 void exercice9() {
 	int n;
-
+	int total = 0;
 	std::cout << "Entrez votre nombre : ";
 	std::cin >> n;
-	std::cout << sizeof(n);
-	//transformer mon nombre en tableau de character
-	//char sep[sizeof(n)];
+	std::string num = std::to_string(n);
+	int length = num.size();
+	int tabNum[25] = {0};
+	for (int i = 0; i < num.size(); i++) {
+		tabNum[i] = num[i] - '0';
+	}
+	for (int i = 0; i<length; i++) {
+		if (i == 0) {
+			std::cout << tabNum[i];
+			total = total + tabNum[i];
 
+		}
+		else {
+			std::cout << " + " << tabNum[i];
+			total = total + tabNum[i];
+		}
+	}
+	std::cout << " = " << total;
+}
 
+void exercice10() {
+	int nombre;
+	std::cout << "Entrer un nombre : ";
+	std::cin >> nombre;
 
+	std::bitset<8> bits(nombre);
+
+	std::cout << bits << std::endl;
 }
 
 void main() {
-	exercice9();
+	exercice10();
 }
---
+++ Labo2/Labo2/Labo.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+void exercice5() {
+
+}
+void main() {
+	exercice5();
+}
